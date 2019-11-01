45e5534bc81352a517bf0dada8e7815972209127
RestoreMonarchy <35507241+RestoreMonarchy@users.noreply.github.com>
2019-11-01 17:37:50 +0100

    Added Vehicle and Gun license

--- RPLicensesConfiguration.cs
+++ RPLicensesConfiguration.cs
@@ -4,12 +4,25 @@ namespace RestoreMonarchy.RPLicenses
 {
     public class RPLicensesConfiguration : IRocketPluginConfiguration
     {
+        public string MessageColor { get; set; }
+        public bool VehicleLicenseEnabled { get; set; }
+        public bool GunLicenseEnabled { get; set; }
         public ushort VehicleLicenseID { get; set; }
         public ushort GunLicenseID { get; set; }
 
+        private readonly string DEFAULT_COLOR = "yellow";
+        private readonly bool DEFAULT_VEHICLE_LICENSE_ENABLED = true;
+        private readonly bool DEFAULT_GUN_LICENSE_ENABLED = true;
+        private readonly ushort DOLLARS_NOTE_10 = 1052;
+        private readonly ushort DOLLARS_NOTE_50 = 1054;
+
         public void LoadDefaults()
         {
-
+            MessageColor = DEFAULT_COLOR;
+            VehicleLicenseEnabled = DEFAULT_VEHICLE_LICENSE_ENABLED;
+            GunLicenseEnabled = DEFAULT_GUN_LICENSE_ENABLED;
+            VehicleLicenseID = DOLLARS_NOTE_10;
+            GunLicenseID = DOLLARS_NOTE_50;
         }
     }
 }
--- RPLicensesPlugin.cs
+++ RPLicensesPlugin.cs
@@ -1,24 +1,86 @@
-﻿using Rocket.Core.Logging;
+﻿using Rocket.API.Collections;
 using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Chat;
 using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using UnityEngine;
+using Logger = Rocket.Core.Logging.Logger;
 
 namespace RestoreMonarchy.RPLicenses
 {
     public class RPLicensesPlugin : RocketPlugin<RPLicensesConfiguration>
     {
+        public Color MessageColor { get; set; }
         protected override void Load()
-        {            
+        {
+            MessageColor = UnturnedChat.GetColorFromName(Configuration.Instance.MessageColor, Color.green);
+            
+            if (Configuration.Instance.VehicleLicenseEnabled)
+            {
+                UnturnedPlayerEvents.OnPlayerUpdateStance += OnPlayerUpdateStance;
+            }            
+            if (Configuration.Instance.GunLicenseEnabled)
+            {
+                U.Events.OnPlayerConnected += OnPlayerConnected;
+                U.Events.OnPlayerDisconnected += OnPlayerDisconnected;
+            }           
+
             Logger.Log($"{Name} {Assembly.GetName().Version} has been loaded!", ConsoleColor.Yellow);
+        }
+
+        private void OnPlayerConnected(UnturnedPlayer player)
+        {
+            player.Player.equipment.onEquipRequested += OnEquipRequested;
+        }
+
+        private void OnPlayerDisconnected(UnturnedPlayer player)
+        {
+            player.Player.equipment.onEquipRequested -= OnEquipRequested;
+        }
+
+        private void OnEquipRequested(PlayerEquipment equipment, ItemJar jar, ItemAsset asset, ref bool shouldAllow)
+        {
+            if (asset.type == EItemType.GUN && equipment.player.inventory.has(Configuration.Instance.GunLicenseID) == null)
+            {
+                shouldAllow = false;
+                UnturnedChat.Say(equipment.player.channel.owner.playerID.steamID, Translate("RequireGunLicense"), MessageColor);
+            }
+        }
+
+        private void OnPlayerUpdateStance(UnturnedPlayer player, byte stance)
+        {
+            if (stance == (byte)EPlayerStance.DRIVING && player.Inventory.has(Configuration.Instance.VehicleLicenseID) == null)
+            {
+                if (player.CurrentVehicle.tryRemovePlayer(out byte seat, player.CSteamID, out Vector3 vector, out byte angle))
+                {
+                    VehicleManager.sendExitVehicle(player.CurrentVehicle, seat, vector, angle, false);
+                    UnturnedChat.Say(player, Translate("RequireDriveLicense"), MessageColor);
+                }
+            }
         }
 
         protected override void Unload()
         {
+            if (Configuration.Instance.VehicleLicenseEnabled)
+            {
+                UnturnedPlayerEvents.OnPlayerUpdateStance -= OnPlayerUpdateStance;
+            }            
+            if (Configuration.Instance.GunLicenseEnabled)
+            {
+                U.Events.OnPlayerConnected -= OnPlayerConnected;
+                U.Events.OnPlayerDisconnected -= OnPlayerDisconnected;
+            }
+
             Logger.Log($"{Name} has been unloaded!", ConsoleColor.Yellow);
         }
+
+        public override TranslationList DefaultTranslations => new TranslationList()
+        {
+            { "RequireDriveLicense", "You must have a license with you to drive vehicle!" },
+            { "RequireGunLicense", "You must have a license with you to equip gun!" }
+        };
     }
 }
