2a7d1dfe3ed5c7b7ceacf0b45b767602198014a2
RestoreMonarchy <35507241+RestoreMonarchy@users.noreply.github.com>
2019-11-01 16:46:13 +0100

    Initial commit

---
+++ RPLicensesConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Rocket.API;
+
+namespace RestoreMonarchy.RPLicenses
+{
+    public class RPLicensesConfiguration : IRocketPluginConfiguration
+    {
+        public ushort VehicleLicenseID { get; set; }
+        public ushort GunLicenseID { get; set; }
+
+        public void LoadDefaults()
+        {
+
+        }
+    }
+}
---
+++ RPLicensesPlugin.cs
@@ -0,0 +1,24 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestoreMonarchy.RPLicenses
+{
+    public class RPLicensesPlugin : RocketPlugin<RPLicensesConfiguration>
+    {
+        protected override void Load()
+        {            
+            Logger.Log($"{Name} {Assembly.GetName().Version} has been loaded!", ConsoleColor.Yellow);
+        }
+
+        protected override void Unload()
+        {
+            Logger.Log($"{Name} has been unloaded!", ConsoleColor.Yellow);
+        }
+    }
+}
